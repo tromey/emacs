72b891723dc3c722121dc75806adcf8403b6f168
Tom Tromey <tom@tromey.com>
2017-03-04 10:53:01 -0700

    fix up the fontifier again

--- lisp/textmodes/css-mode.el
+++ lisp/textmodes/css-mode.el
@@ -1039,30 +1039,35 @@ be fontified with a red background."
 (defun css--fontify-region (start end &optional loudly)
   "Fontify a CSS buffer between START and END.
 START and END are buffer positions."
-  (font-lock-default-fontify-region start end loudly)
-  (when css-fontify-colors
-    (save-excursion
-      (let ((case-fold-search t))
-        (goto-char start)
-        (while (re-search-forward css--colors-regexp end t)
-          ;; Skip comments and strings.
-          (unless (nth 8 (syntax-ppss))
-            (let ((start (match-beginning 0))
-                  (color (css--compute-color)))
-              (when color
-                (with-silent-modifications
-                  ;; Use the color as the background, to make it more
-                  ;; clear.  Use a contrasting color as the foreground,
-                  ;; to make it readable.  Finally, have a small box
-                  ;; using the existing foreground color, to make sure
-                  ;; it stands out a bit from any other text; in
-                  ;; particular this is nice when the color matches the
-                  ;; buffer's background color.
-                  (add-text-properties
-                   start (point)
-                   (list 'face (list :background color
-                                     :foreground (css--contrasty-color color)
-                                     :box '(:line-width -1)))))))))))))
+  (let ((extended-region (font-lock-default-fontify-region start end loudly)))
+    (when css-fontify-colors
+      (when (and (consp extended-region)
+		 (eq (car extended-region) 'jit-lock-bounds))
+	(setq start (cadr extended-region))
+	(setq end (cddr extended-region)))
+      (save-excursion
+	(let ((case-fold-search t))
+	  (goto-char start)
+	  (while (re-search-forward css--colors-regexp end t)
+	    ;; Skip comments and strings.
+	    (unless (nth 8 (syntax-ppss))
+	      (let ((start (match-beginning 0))
+		    (color (css--compute-color)))
+		(when color
+		  (with-silent-modifications
+		    ;; Use the color as the background, to make it more
+		    ;; clear.  Use a contrasting color as the foreground,
+		    ;; to make it readable.  Finally, have a small box
+		    ;; using the existing foreground color, to make sure
+		    ;; it stands out a bit from any other text; in
+		    ;; particular this is nice when the color matches the
+		    ;; buffer's background color.
+		    (add-text-properties
+		     start (point)
+		     (list 'face (list :background color
+				       :foreground (css--contrasty-color color)
+				       :box '(:line-width -1))))))))))))
+    extended-region))
 
 (defcustom css-indent-offset 4
   "Basic size of one indentation step."
